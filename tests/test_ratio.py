24d21d8628ba153a84779a57b42375b85560481b
Jia Chunying <iccccing@gmail.com>
2020-04-05 11:15:45 +0800

    2020.4.5 v1.0  Add UT #19,

--- tests/test_ratio.py
+++ tests/test_ratio.py
@@ -2,56 +2,44 @@ import pytest
 
 
 @pytest.mark.skipif('True==True')
-@pytest.mark.usefixtures('client', 'token', 'header_key')
+@pytest.mark.usefixtures('client', 'headers')
 class TestRatio:
 
-    def test_ratio_gender(self, client, token, header_key):
+    def test_ratio_gender(self, client, headers):
         payload = {
             'country': 'China',
             'stime': '2020-03-24',
             'etime': '2020-03-22'
-        }
-        headers = {
-            header_key: token,
         }
         response = client.get('/ratio/gender', params=payload, headers=headers)
         print("response: ", response)
         assert response.status_code == 200
 
-    def test_ratio_age(self, client, token, header_key):
+    def test_ratio_age(self, client, headers):
         payload = {
             'country': 'China',
             'stime': '2020-03-24',
             'etime': '2020-03-22'
-        }
-        headers = {
-            header_key: token,
         }
         response = client.get('/ratio/age', params=payload, headers=headers)
         print("response: ", response)
         assert response.status_code == 200
 
-    def test_ratio_rehabilitation(self, client, token, header_key):
+    def test_ratio_rehabilitation(self, client, headers):
         payload = {
             'country': 'China',
             'stime': '2020-03-24',
             'etime': '2020-03-22'
-        }
-        headers = {
-            header_key: token,
         }
         response = client.get('/ratio/rehabilitatio', params=payload, headers=headers)
         print("response: ", response)
         assert response.status_code == 200
 
-    def test_ratio_sars(self, client, token, header_key):
+    def test_ratio_sars(self, client, headers):
         payload = {
             'country': 'China',
             'stime': '2020-03-24',
             'etime': '2020-03-22'
-        }
-        headers = {
-            header_key: token,
         }
         response = client.get('/ratio/sars', params=payload, headers=headers)
         print("response: ", response)
